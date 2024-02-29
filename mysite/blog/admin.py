46d36826b6080aa92e4e87fb838fb60dc9fb8710
intel <olxdane@gmail.com>
2024-02-29 14:23:51 +0200

    Initial commit

---
+++ mysite/blog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post,PostPoint
+admin.site.register(Post)
+admin.site.register(PostPoint)
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
