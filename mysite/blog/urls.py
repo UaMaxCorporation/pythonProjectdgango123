27842278adf8b08007467bc16bc43ebd590d8d97
intel <olxdane@gmail.com>
2024-02-29 14:55:42 +0200

    add login

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     #path('', views.PostListViev.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-    path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag')
+    path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
+    path('login/', views.user_login, name='login'),
 ]
